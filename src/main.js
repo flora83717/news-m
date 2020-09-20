7f2b585a3d06e693953cdeed47381791876313f6
Elodie <flora83717@gmail.com>
2020-09-20 21:28:21 +0800

    項目初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加載全局樣式
+import style from './styles/index.scss'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+
 
 Vue.config.productionTip = false
-
+Vue.use(Vant)
 new Vue({
   router,
   store,
